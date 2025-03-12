46a773cbcd801db3eb5b9fb6f16cd2afdf2e4169
LegendBug <1278998559@qq.com>
2025-03-12 14:26:13 +0800

    test: add the broadcaster test demo (have bug)

--- app/api/router.py
+++ app/api/router.py
@@ -1,6 +1,6 @@
 # 用于注册各模块路由
 from fastapi import APIRouter
-from app.api.endpoints import user, auth
+from app.api.endpoints import user, auth, chat
 
 # 创建主路由
 router = APIRouter()
@@ -8,3 +8,4 @@ router = APIRouter()
 # 注册各模块路由
 router.include_router(auth.router, prefix="/auth", tags=["认证"])
 router.include_router(user.router, prefix="/user", tags=["用户"])
+router.include_router(chat.router, prefix="/chat", tags=["群聊"])
