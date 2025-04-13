58f75f83d8b06ef6b6a56b2e8d07a6a7f978fb65
LegendBug <1278998559@qq.com>
2025-04-13 15:30:43 +0800

    feat: inform member info when joining/leaving project

--- app/api/endpoints/project/websocket.py
+++ app/api/endpoints/project/websocket.py
@@ -5,17 +5,23 @@ from fastapi import APIRouter, Depends, WebSocket, WebSocketDisconnect
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.api.deps import get_current_project, get_db, get_current_user_ws
-from app.models.project.project import Project
+from app.models.project.project import MemberInfo, Project
 from app.models.user import User
 from app.repositories.project.project import ProjectDAO
 from loguru import logger
 
 from pydantic import ValidationError
 from app.models.project.websocket import (
-    ClientMessage, BaseMessage, Message, EventScope, MemberAction,
-    WrongInputMessageFormatErrorStr, ScopeNotAllowedErrorStr
+    ClientMessage,
+    BaseMessage,
+    Message,
+    EventScope,
+    MemberAction,
+    WrongInputMessageFormatErrorStr,
+    ScopeNotAllowedErrorStr,
 )
 from .websocket_handlers import project_general_manager, get_project_channel_name
+
 router = APIRouter()
 
 
@@ -30,7 +36,7 @@ async def project(
     The all in one manner greatly reduces the websocket connection number."""
     await websocket.accept()
 
-    # check user permission 
+    # check user permission
     try:
         is_member = await ProjectDAO.is_project_member(current_project, current_user, db)
         if not is_member:
@@ -42,16 +48,27 @@ async def project(
         await websocket.close(code=4000, reason="Server error")
         return
 
+    member_info = MemberInfo(
+        user_id=current_user.id,
+        username=current_user.username,
+        email=current_user.email,
+        permission=await ProjectDAO.get_project_permission(current_project, current_user, db),
+    )
+
     # connect user to this project channel
     client_id = str(current_user.id)
     channel = get_project_channel_name(current_project.id)
     try:
         await project_general_manager.subscribe(client_id, channel, websocket)
-        await project_general_manager.publish(channel, ClientMessage(
-                client_id = client_id,
-                scope = EventScope.MEMBER,
-                action = MemberAction.JOINED,
-            ).model_dump_json())
+        await project_general_manager.publish(
+            channel,
+            ClientMessage(
+                client_id=client_id,
+                scope=EventScope.MEMBER,
+                action=MemberAction.JOINED,
+                payload=member_info.model_dump(),
+            ).model_dump_json(),
+        )
     except Exception as e:
         logger.error(f"Error connecting to project: {e}")
         await project_general_manager.disconnect(client_id)
@@ -61,26 +78,32 @@ async def project(
     try:
         while True:
             raw_message = await websocket.receive_text()
-            
-            try: 
+
+            try:
                 message = BaseMessage(**orjson.loads(raw_message))
             except (orjson.JSONDecodeError, ValidationError) as e:
                 await websocket.send_text(WrongInputMessageFormatErrorStr)
                 continue
-            
-            if message.scope in (EventScope.CHAT, EventScope.CRDT, ):
-                await project_general_manager.publish(channel, Message(
-                    client_id = client_id,
-                    **message.model_dump()
-                ).model_dump_json())
+
+            if message.scope in (
+                EventScope.CHAT,
+                EventScope.CRDT,
+            ):
+                await project_general_manager.publish(
+                    channel, Message(client_id=client_id, **message.model_dump()).model_dump_json()
+                )
             else:
                 await websocket.send_text(ScopeNotAllowedErrorStr)
     except WebSocketDisconnect:
         await project_general_manager.disconnect(client_id)
-        await project_general_manager.publish(channel, ClientMessage(
-            client_id = client_id,
-            scope = EventScope.MEMBER,
-            action = MemberAction.LEFT,
-        ).model_dump_json())
-        
+        await project_general_manager.publish(
+            channel,
+            ClientMessage(
+                client_id=client_id,
+                scope=EventScope.MEMBER,
+                action=MemberAction.LEFT,
+                payload=member_info.model_dump(),
+            ).model_dump_json(),
+        )
+
         logger.debug(f"Client {client_id} disconnected from project {current_project.id}")
