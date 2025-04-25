27a77426df2f574e85b99a7f679cbe7628f042d8
meowking <mr.meowking@posteo.com>
2025-04-25 10:37:18 +0800

    ci: deployment

--- misc/scripts/run-in-venv.sh
+++ misc/scripts/run-in-venv.sh
@@ -3,25 +3,10 @@
 set -euo pipefail
 
 project_dir="/var/www/hivey/backend/"
-
 cd "$project_dir"
-
-if [[ -d ./.venv ]]; then
-    # since `rsync --delete` will empty most of the stuffs inside .venv (not not all)
-    rm -rf ./.venv
-fi
-
-python3 -m venv .venv
 
 . .venv/bin/activate
 
-echo "Updating/Install dependencies..."
-pip3 install -r ./requirements_full.txt
-
-echo "Starting Service..."
-CPU_CORES=$(nproc)
-WORKERS=$((2 * CPU_CORES + 1))
-# FIXME temporarily we cannot run with workers > 1
-fastapi run --workers 1 ./app
+exec "$@"
 
 
--- misc/scripts/update-systemd-service.sh
+++ misc/scripts/update-systemd-service.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
 systemctl disable --now hivey-back-end 2>/dev/null
-rm -f /usr/lib/systemd/system/hivey-back-end.service
-cp -f /var/www/hivey/backend/misc/conf/hivey-back-end.service /usr/lib/systemd/system/hivey-back-end.service
+rm -f /usr/lib/systemd/system/hivey-back.service
+rm -f /usr/lib/systemd/system/hivey-back-workder.service
+rm -f /usr/lib/systemd/system/hivey-back-api.service
+cp -f /var/www/hivey/backend/misc/conf/hivey-back.service /usr/lib/systemd/system/hivey-back.service
+cp -f /var/www/hivey/backend/misc/conf/hivey-back-api.service /usr/lib/systemd/system/hivey-back-api.service
+cp -f /var/www/hivey/backend/misc/conf/hivey-back-worker.service /usr/lib/systemd/system/hivey-back-worker.service
+
 systemctl daemon-reload
-systemctl enable --now hivey-back-end
+systemctl enable --now hivey-back
