b10a52f5d8caad9644e8bbb817335016458b2abf
meowking <mr.meowking@posteo.com>
2025-04-25 10:50:06 +0800

    fix(ci)

--- misc/scripts/update-systemd-service.sh
+++ misc/scripts/update-systemd-service.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 systemctl disable --now hivey-back-end 2>/dev/null
-rm -f /usr/lib/systemd/system/hivey-back.service
+rm -f /usr/lib/systemd/system/hivey-back.target
 rm -f /usr/lib/systemd/system/hivey-back-workder.service
 rm -f /usr/lib/systemd/system/hivey-back-api.service
-cp -f /var/www/hivey/backend/misc/conf/hivey-back.service /usr/lib/systemd/system/hivey-back.service
+cp -f /var/www/hivey/backend/misc/conf/hivey-back.service /usr/lib/systemd/system/hivey-back.target
 cp -f /var/www/hivey/backend/misc/conf/hivey-back-api.service /usr/lib/systemd/system/hivey-back-api.service
 cp -f /var/www/hivey/backend/misc/conf/hivey-back-worker.service /usr/lib/systemd/system/hivey-back-worker.service
 
